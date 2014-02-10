bf12b6eab7a0b2abb31f96e6be4ebc3468cebf65
Samuel French <samfrench@gmail.com>
2014-02-09 19:49:44 -0600

    added routes for git functions, also added closing php tag

--- app/routes.php
+++ app/routes.php
@@ -29,3 +29,74 @@ Route::get('user/{user}/project/{project}/editor', function($user, $project)
 Route::pattern('file', '.*');
 Route::resource('user.project.file', 'FileController');
 Route::post('user/{user}/project/{project}/files', 'FileController@indexPost');
+
+/*
+Branch Operations:Commit Operations
+*/
+Route::post('/user/{user}/project/{project}/git-add', GitController@stageFiles);
+
+Route::post('/user/{user}/project/{project}/git-commit', GitController@commit);
+
+/*
+Branch Operations:Push
+*/
+Route::post('/user/{user}/project/{project}/git-push', GitController@push);
+
+/*
+Branch Operations:Merge
+*/
+Route::post('/user/{user}/project/{project}/git-merge', GitController@merge);
+
+/*
+Branch Operations:Pull Changes
+*/
+Route::post('/user/{user}/project/{project}/git-pull', GitController@pull);
+
+/*
+Branch Operation:Retrieve List
+*/
+Route::get('/user/{user}/project/{project}/git-branch', GitController@getBranch);
+
+/*
+Branch Operations:Create New
+*/
+Route::post('/user/{user}/project/{project}/git-branch', GitController@createBranch);
+
+/*
+Branch Operations:Delete
+*/
+Route::delete('/user/{user}/project/{project}/git-branch', GitController@deleteBranch);
+
+/*
+Branch Operations:Checkout
+*/
+Route::post('/user/{user}/project/{project}/git-checkout', GitController@checkoutBranch);
+
+/*
+Branch Operations:Retrieve Past Commits (Current Branch)
+*/
+Route::get('/user/{user}/project/{project}/git-log', GitController@listBranchCommits);
+
+/*
+Repository Operations
+Repository Operations:Download Changes
+*/
+Route::post('/user/{user}/project/{project}/git-fetch', GitController@downloadChanges);
+
+/*
+Repository Operations:List Remote Repositories
+*/
+Route::get('/user/{user}/project/{project}/git-remote', GitController@listRemoteRepos);
+
+/*
+Repository Operations:Edit
+*/
+Route::post('/user/{user}/project/{project}/git-remote', GitController@createNewRemoteRepo);
+
+Route::delete('/user/{user}/project/{project}/git-remote', GitController@deleteRemoteRepo);
+
+/*
+Custom Command Handling
+*/
+Route::post('/user/{user}/project/{project}/git', GitController@customCmd);
+?>
