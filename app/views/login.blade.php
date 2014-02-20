5d7383919170fac24719762b695e38ac94b52403
wwforg <wwforg@noctrl.edu>
2014-02-20 11:45:59 -0600

    Hopeful Changes

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -3,9 +3,13 @@
 @section('content')
 	{{ HTML::style('css/login.css') }}
 	<body background="{{ URL::asset('css/images/background.png') }}">
-	<?php
-		LoginController::GitHubLogin();
-	?>
+	
+	<script>
+		function loginCall() {
+			<?php LoginController:GitHubLogin(); ?>
+		}
+	</script>
+	
         <div class="container">
 		<BR/><BR/><BR/><BR/><BR/><BR/><BR/>
 		<div class="panel panel-success">
@@ -13,7 +17,7 @@
 			<div class="panel-body">
 				<form class="form-signin" role="form">
 					<h3 class="form-signin-heading">Englewood Codes</h3>
-					<button class="btn btn-lg btn-signIn btn-block" type="submit">Login with GitHub</button>
+					<button class="btn btn-lg btn-signIn btn-block" type="submit" onclick="loginCall()">Login with GitHub</button>
 					<hr>
 					<a href ="https://github.com/" class="btn btn-med btn-GitHub btn-block" type="button">Create GitHub Account</a>
 				</form>
