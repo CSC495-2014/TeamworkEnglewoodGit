0d441209c08b3470da91f57926b06ee36ae394f3
wwforg <wwforg@noctrl.edu>
2014-02-20 11:32:46 -0600

    Testing

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -3,7 +3,9 @@
 @section('content')
 	{{ HTML::style('css/login.css') }}
 	<body background="{{ URL::asset('css/images/background.png') }}">
-	
+	<?php
+		LoginController::GitHubLogin
+	?>
         <div class="container">
 		<BR/><BR/><BR/><BR/><BR/><BR/><BR/>
 		<div class="panel panel-success">
