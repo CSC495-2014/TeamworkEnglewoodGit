e9fa73f41898383ecdd6607ed7219ca802d40a3e
wwforg <wwforg@noctrl.edu>
2014-02-28 02:13:03 -0600

    testing config pt 2

--- app/models/Login.php
+++ app/models/Login.php
@@ -15,11 +15,13 @@ class Login
     
     function __construct()
     {
+        echo "<script type='text/javascript'>alert('Testing Config File');</script>";
+        //Config::get('login.variable_name');
 	$this->provider = new OAuth2\Client\Provider\Github(array(
-	    'clientId' => 'fd0b49991778467ebe9d',
-	    'clientSecret' => '82c139b5cf2109a8b9ae0670fd0d818640f1b3bc',
-	    'redirectUri' => 'http://54.200.185.101/login',
-	    'scopes' => array('user','repo', 'admin:public_key', 'read:org')
+	    'clientId' => Config::get(login.clientId),
+	    'clientSecret' => Config::get(login.clientSecret),
+	    'redirectUri' => Config::get(login.redirectUri),
+	    'scopes' => Config::get(login.scopes)
 	));
         if(!isset($_GET['code']))
 	{
