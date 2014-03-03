e6414d11481941f4a629e03db3f98bc475e67641
wwforg <wwforg@noctrl.edu>
2014-03-03 16:28:47 -0600

    GitHub API call test 4

--- app/models/Login.php
+++ app/models/Login.php
@@ -135,7 +135,9 @@ class Login
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
 
         // $output contains the output string 
-        $output = curl_exec($ch); 
+        $output = curl_exec($ch);
+	
+	var_deump($output);
 
         // close curl resource to free up system resources 
         curl_close($ch);
