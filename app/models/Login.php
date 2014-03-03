ef7e8b17d93ecf96f8ade7023076f68bae3a4544
wwforg <wwforg@noctrl.edu>
2014-03-03 17:00:19 -0600

    GitHub API call test 15: user agent correction

--- app/models/Login.php
+++ app/models/Login.php
@@ -128,11 +128,13 @@ class Login
         $ch = curl_init(); 
 
         // set url 
-        curl_setopt($ch, CURLOPT_URL, "'User-Agent: wwforg'https://api.github.com/orgs/CSC495-2014/members");
+        curl_setopt($ch, CURLOPT_URL, "https://api.github.com/orgs/CSC495-2014/members");
 	///
 
         //return the transfer as a string 
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	
+	curl_setopt($ch, CURLOPT_USERAGENT, "wwforg");
 
 	if(curl_exec($ch) === false)
 	{
