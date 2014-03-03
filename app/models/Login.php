c2c69bfa2ebd669d0895746b781f22993b03a434
wwforg <wwforg@noctrl.edu>
2014-03-03 16:30:58 -0600

    GitHub API call test 6: user agent

--- app/models/Login.php
+++ app/models/Login.php
@@ -128,7 +128,7 @@ class Login
         $ch = curl_init(); 
 
         // set url 
-        curl_setopt($ch, CURLOPT_URL, "https://api.github.com/orgs/CSC495-2014/members");
+        curl_setopt($ch, CURLOPT_URL, "'User-Agent: ' https://api.github.com/orgs/CSC495-2014/members");
 	///$this->userName?access_token=$this->token
 
         //return the transfer as a string 
