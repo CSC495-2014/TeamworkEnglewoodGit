0b9c6b29939d0f8cf2925d18878d36c6f3ca42dd
wwforg <wwforg@noctrl.edu>
2014-03-03 19:02:07 -0600

    update loginController

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -4,7 +4,7 @@ class LoginController extends BaseController {
 
     /**
     *
-    * Drive the Login Process
+    * Drive the Login Process, online or offline depending on config variable
     *
     */
     public static function GitHubLogin()
@@ -19,27 +19,12 @@ class LoginController extends BaseController {
 	    $userName = Config::get('oauth.offlineUserName');
 	    $userId = Config::get('oauth.offlineTableId');
 	    $token = Config::get('oauth.offlineToken');
+	    
 	    Session::put('uid', $userName);
 	    Session::put('tableId', $userId);
 	    Session::put('token', $token);
 	    
-	    $url = URL::to('user/testing/projects');
-	    
-	    //return Redirect::to('user/{user}/projects', ['user' => $userName]);
-	    //return Redirect::to('user/{user}/projects', [$userName]);
-	    //return Redirect::route('user/{user}/projects', $userName);
-	    
-	    //$hasTable = Schema::hasTable('users');
-	    
-	    //$results = DB::select('select * from users where $userName = username', array(1));
-	    
-	    //$results = DB::select('select * from users where user_id = 1', array(1));
-	    
-	    //$userExists = DB::select('users')->where('username',$userName)->find();
-	    //var_dump($results);
-	    //echo "<script type='text/javascript'>alert('User Exists: $results[0]');</script>";
-	    
-	    //Redirect::route('user/$userName/projects');
+	    //Route to Projects Page
 	}
     }
 }
