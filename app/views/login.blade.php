9da8656ac717357b40b3199f1a098f7097b1e309
wwforg <wwforg@noctrl.edu>
2014-03-04 11:27:20 -0600

    online configuration

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -4,8 +4,7 @@
 	{{ HTML::style('css/login.css') }}
 	<body background="{{ URL::asset('css/images/background.png') }}">
 	
-	<?php
-	include(app_path().'/controllers/LoginController.php');
+	<?php include(app_path().'/controllers/LoginController.php');
 	//function loginCall()
 	//{
 		LoginController::GitHubLogin();
@@ -19,7 +18,7 @@
 			<div class="panel-body">
 				<form class="form-signin" role="form">
 					<h3 class="form-signin-heading">Englewood Codes</h3>
-					<button class="btn btn-lg btn-signIn btn-block" type="submit" name="loginButton" value="loginButton" >Login with GitHub</button>
+					<button class="btn btn-lg btn-signIn btn-block" type="submit" >Login with GitHub</button>
 					<hr>
 					<a href ="https://github.com/" class="btn btn-med btn-GitHub btn-block" type="button">Create GitHub Account</a>
 				</form>
