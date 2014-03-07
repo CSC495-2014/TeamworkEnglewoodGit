109c0aa4a307073de6ac52f51564e55e9ce6929f
wwforg <wwforg@notrl.edu>
2014-03-06 19:44:19 -0600

    Replacing Curl request with HTTP request 3

--- app/models/Login.php
+++ app/models/Login.php
@@ -22,33 +22,33 @@ class Login
     */
     function __construct()
     {
-	echo "<script type='text/javascript'>alert('FWEIOJFWIOFWE');</script>";
-	$this->provider = $this->_getProvider();
-        $this->organization = Config::get('oauth.organization');
-        if(!isset($_GET['code']))
-	{
-	    $this->provider->authorize();
-	}
-        else
-        {
-            try
-            {
-                $this->token = $this->_getToken();
-                try
-                {
-                    $this->userDetails = $this->_getDetails();
-		    $this->userName = $this->userDetails->nickname;
-                }
-                catch(Exception $e)
-                {
-                    echo "<script type='text/javascript'>alert('Failed to get User Details');</script>";
-                }
-            }
-            catch(Exception $e)
-            {
-                echo "<script type='text/javascript'>alert('Failed to get Access Token');</script>";
-            }
-        }
+		echo "<script type='text/javascript'>alert('FWEIOJFWIOFWE');</script>";
+		$this->provider = $this->_getProvider();
+			$this->organization = Config::get('oauth.organization');
+			if(!isset($_GET['code']))
+		{
+			$this->provider->authorize();
+		}
+			else
+			{
+				try
+				{
+					$this->token = $this->_getToken();
+					try
+					{
+						$this->userDetails = $this->_getDetails();
+				$this->userName = $this->userDetails->nickname;
+					}
+					catch(Exception $e)
+					{
+						echo "<script type='text/javascript'>alert('Failed to get User Details');</script>";
+					}
+				}
+				catch(Exception $e)
+				{
+					echo "<script type='text/javascript'>alert('Failed to get Access Token');</script>";
+				}
+			}
     }
     
     /**
@@ -64,7 +64,7 @@ class Login
 	    'clientSecret' => Config::get('oauth.clientSecret'),
 	    'redirectUri' => Config::get('oauth.redirectUri'),
 	    'scopes' => Config::get('oauth.scopes')
-	));
+		));
     }
     
     /**
@@ -97,45 +97,45 @@ class Login
     */
     public function processUser()
     {
-	//$userExists = $this->userExists();
-	$userInGroup = $this->_checkUserGroup();
-	
-	if($userInGroup)
-	{
-	    echo "<script type='text/javascript'>alert('In Group');</script>";
-	    echo "<script type='text/javascript'>alert('Login for $this->userName');</script>";
-	    /*
-	    if(!is_null($userExists))
-	    {
-	    $this->tableId = $userExists
-		echo "<script type='text/javascript'>alert('In Group, In Table');</script>";
-	    }
-	    else
-	    {
-		echo "<script type='text/javascript'>alert('In Group, Not In Table');</script>";
-		//Database Query to Add User Goes Here
-	    }
-	    $this->beginSession();
-	    return array (, $this->userName, $this->tableId, $this->token);
-	    */
-	}
-	else
-	{
-	    echo "<script type='text/javascript'>alert('Not In Group');</script>";
-	    /*
-	    if(!is_null($userExists))
-	    {
-		//Database Query to Delete User Goes Here
-		echo "<script type='text/javascript'>alert('Login Failed: Not a member of group. User deleted');</script>";
-		//Route to Login Page
-	    }
-	    else
-	    {
-		echo "<script type='text/javascript'>alert('Login Failed: Not a member of group');</script>";
-		//Route to Login Page
-	    }
-	    */
-	}
+		//$userExists = $this->userExists();
+		$userInGroup = $this->_checkUserGroup();
+		
+		if($userInGroup)
+		{
+			echo "<script type='text/javascript'>alert('In Group');</script>";
+			echo "<script type='text/javascript'>alert('Login for $this->userName');</script>";
+			/*
+			if(!is_null($userExists))
+			{
+			$this->tableId = $userExists
+			echo "<script type='text/javascript'>alert('In Group, In Table');</script>";
+			}
+			else
+			{
+			echo "<script type='text/javascript'>alert('In Group, Not In Table');</script>";
+			//Database Query to Add User Goes Here
+			}
+			$this->beginSession();
+			return array (, $this->userName, $this->tableId, $this->token);
+			*/
+		}
+		else
+		{
+			echo "<script type='text/javascript'>alert('Not In Group');</script>";
+			/*
+			if(!is_null($userExists))
+			{
+			//Database Query to Delete User Goes Here
+			echo "<script type='text/javascript'>alert('Login Failed: Not a member of group. User deleted');</script>";
+			//Route to Login Page
+			}
+			else
+			{
+			echo "<script type='text/javascript'>alert('Login Failed: Not a member of group');</script>";
+			//Route to Login Page
+			}
+			*/
+		}
     }
     
     /**
@@ -147,32 +147,35 @@ class Login
     */
     private function _checkUserGroup()
     {
-	$request = Request::get('https://api.github.com/users/$this->userName/orgs?access_token=$this->token');
-	$resultsArray=json_decode($request->body, true);
-	
-	echo "<script type='text/javascript'>alert('Successful HTTP request');</script>";
-	
-	for ($x=0; $x<count($resultsArray); $x++)
-	{
-	    if (in_array($this->organization, $resultsArray{$x})) {
-		return true;
-	    }
-	}
-	return false;
+		$request = new Symfony\Component\HttpFoundation\Request(array(
+			'HTTP_USER_AGENT' => 'TeamworkEnglewoodGit'														  
+		));
+		$request = Request::get('https://api.github.com/users/$this->userName/orgs?access_token=$this->token');
+		$resultsArray=json_decode($request->body, true);
+		
+		echo "<script type='text/javascript'>alert('Successful HTTP request');</script>";
+		
+		for ($x=0; $x<count($resultsArray); $x++)
+		{
+			if (in_array($this->organization, $resultsArray{$x})) {
+			return true;
+			}
+		}
+		return false;
     }
     
     public function getUserName()
     {
-	return $this->userName;
+		return $this->userName;
     }
     
     public function getTableId()
     {
-	return $this->tableId;
+		return $this->tableId;
     }
     
     public function getToken()
     {
-	return $this->token;
+		return $this->token;
     }
 }
