8f8ec7547792298a82966c39a789827f78bb1205
wwforg <wwforg@noctrl.edu>
2014-03-07 09:17:10 -0600

    changing settings for loop test 2

--- app/config/oauth.php
+++ app/config/oauth.php
@@ -105,6 +105,6 @@ return array(
     | The name of the organization to confirm membership with
     |
     */
-    'organization' => 'TeamworkEnglewoodGitTestingTwo'
+    'organization' => 'TeamworkEnglewoodTestingTwo'
   
 );
