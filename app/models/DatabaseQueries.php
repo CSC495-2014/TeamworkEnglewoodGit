528f84192a425a12a52105a59686bc2eccd2e459
recrotteau <recrotteau@noctrl.edu>
2014-03-07 15:50:49 -0600

    The queries for the database table Users

--- app/models/DatabaseQueries.php
+++ app/models/DatabaseQueries.php
@@ -2,6 +2,7 @@
 
 class DatabaseQueries extends Eloquent{
 	
+	//Insert a user into the Users table
 	public function insertUsers(string $str1, string $str2) 
 	{		
 	
@@ -10,21 +11,25 @@ class DatabaseQueries extends Eloquent{
 		);
 	}
 	
+	//Delete a user from the Users table
 	public function deleteUsers(string $str1) {
 		
 		DB::table('users')->where('username', '$str1')->delete();
 	}
 	
+	//Fetch the specified user's ID
 	public function getUserId(string $str1) {
 		
 		return DB::table('users')->where('username', '$str1')->pluck('user_id');
 	}
 	
+	//Fetch the specified user's email
 	public function getUserEmail(string $str1) {
 		
 		return DB::table('users')->where('username', '$str1')->pluck('useremail');
 	}	
 	
+	//Find out if a user exits in the Users table
 	public function userExists(string $str1) {
 		
 		DB::table('users')->whereExists(function($query)
