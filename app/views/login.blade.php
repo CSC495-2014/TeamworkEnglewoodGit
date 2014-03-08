ddf1dff5d33d9014206f11fe4d10a8007ff76a4a
wwforg <wwforg@noctrl.edu>
2014-03-07 18:55:28 -0600

    testing button

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -11,7 +11,7 @@
 			<div class="panel-body">
 				<form class="form-signin" role="form">
 					<h3 class="form-signin-heading">Englewood Codes</h3>
-						<button id = "submit" class="btn btn-lg btn-signIn btn-block" onClick="54.200.185.101/login">Login with GitHub</button>
+						<button id = "submit" class="btn btn-lg btn-signIn btn-block" href="54.200.185.101/login">Login with GitHub</button>
 					<hr>
 					<a href ="https://github.com/" class="btn btn-med btn-GitHub btn-block" type="button">Create GitHub Account</a>
 				</form>
