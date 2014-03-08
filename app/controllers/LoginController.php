274fefedce2d2bee2916b012946ce269d4e12081
wwforg <wwforg@noctrl.edu>
2014-03-07 18:18:06 -0600

    testing routing

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -26,7 +26,7 @@ class LoginController extends BaseController {
 			echo "<script type='text/javascript'>alert('Populated Session');</script>";
 			//Route to Projects Page
 			echo "<script type='text/javascript'>alert('Attempting Route');</script>";
-			return Redirect::route('/user/wwforg/projects');
+			Redirect::route('user/wwforg/projects');
 			echo "<script type='text/javascript'>alert('Failed Route');</script>";
 			//return Redirect::route('user/{user}/projects', [$userName]);
 			//return Redirect::route('user/{user}/projects', $userName);
