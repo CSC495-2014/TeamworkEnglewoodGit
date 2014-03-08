aa37adb61f5fb2d0d625e898ef691f7725777b6a
Rachel Crotteau <recrotteau@noctrl.edu>
2014-03-07 23:35:46 -0600

    Update DatabaseQueries.php

--- app/models/DatabaseQueries.php
+++ app/models/DatabaseQueries.php
@@ -1,44 +1,66 @@
 <?php
 
-class DatabaseQueries extends Eloquent{
-	
-	//Insert a user into the Users table
-	public function insertUsers(string $str1, string $str2) 
+class DatabaseQueries{
+	/**
+	* Insert a user into the Users table
+	* 
+	* @param string $userName
+        * @param string $email
+	* @returns user_id
+	*/
+	public function insertUsers(string $userName, string $email) 
 	{		
-	
 		return DB::table('users')->insertGetId(
-		array('username' => '$str1', 'useremail' => '$str2')
+		array('username' => '$userName', 'useremail' => '$email')
 		);
 	}
 	
-	//Delete a user from the Users table
-	public function deleteUsers(string $str1) {
+	/**
+	* Delete a user from the Users table
+	* 
+        * @param string $userName
+	*/
+	public function deleteUsers(string $userName) {
 		
-		DB::table('users')->where('username', '$str1')->delete();
+		DB::table('users')->where('userName', '$name')->delete();
 	}
 	
-	//Fetch the specified user's ID
-	public function getUserId(string $str1) {
+	/**
+	* Fetch the specified user's ID
+	* 
+        * @param string $userName
+	* @returns user_id
+	*/
+	public function getUserId(string $userName) {
 		
-		return DB::table('users')->where('username', '$str1')->pluck('user_id');
+		return DB::table('users')->where('username', '$userName')->pluck('user_id');
 	}
 	
-	//Fetch the specified user's email
-	public function getUserEmail(string $str1) {
+	/**
+	* Fetch the specified user's email
+	* 
+        * @param string $userName
+	* @returns useremail
+	*/
+	public function getUserEmail(string $userName) {
 		
-		return DB::table('users')->where('username', '$str1')->pluck('useremail');
+		return DB::table('users')->where('username', '$userName')->pluck('useremail');
 	}	
 	
-	//Find out if a user exits in the Users table
-	public function userExists(string $str1) {
+	/** 
+	* Find if a user exits in the Users table
+	* 
+        * @param string $userName
+	* @returns user_id (if they do exist)
+	*/
+	public function userExists(string $userName) {
 		
 		DB::table('users')->whereExists(function($query)
 		{
 			$query->select('user_id')
 			->from('users')
-			->whereRaw('username = $str1');
+			->whereRaw('username = $userName');
 		})
 		->get();
 	}
 }
-
