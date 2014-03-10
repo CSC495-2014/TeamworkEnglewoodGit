7a729dd8853d9c55c82c4dbbd0a6ac2db8fdab81
wwforg <wwforg@noctrl.edu>
2014-03-10 14:56:52 -0500

    testing routes

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -54,7 +54,7 @@ class LoginController extends BaseController {
 			Session::put('token', $token);
 			
 			echo "<script type='text/javascript'>alert('Attempting Route');</script>";
-			return Redirect::route('user/wwforg/projects');
+			return Redirect::to('user/wwforg/projects');
 			echo "<script type='text/javascript'>alert('Route Failed');</script>";
 			//Route to Projects Page
 		}
