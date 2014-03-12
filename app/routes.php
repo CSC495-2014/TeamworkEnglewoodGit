3fbeb5eee9e2e210fdd2126820ff5553a943f1fd
wwforg <wwforg@noctrl.edu>
2014-03-12 11:00:21 -0500

    adding route get

--- app/routes.php
+++ app/routes.php
@@ -22,6 +22,7 @@ Route::get('/', function()
 //});
 
 Route::post('login', 'LoginController@gitHubLoginPost');
+Route::get('login', 'LoginController@gitHubLoginPost');
 
 Route::get('user/{user}/project/{project}/editor', function($user, $project)
 {
