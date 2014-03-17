5f52252c9ce01459593b6820d094c87a43c296d7
wwforg <wwforg@noctrl.edu>
2014-03-16 20:19:26 -0500

    testing database on server

--- app/config/database.php
+++ app/config/database.php
@@ -54,7 +54,7 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => '54.200.185.101',
+			'host'      => '',
 			'database'  => 'laravel_db',
 			'username'  => 'root',
 			'password'  => '',
