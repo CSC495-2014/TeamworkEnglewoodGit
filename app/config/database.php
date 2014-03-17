7ca8da13a44eb3bf054bdba21194603b9ddc6b07
wwforg <wwforg@noctrl.edu>
2014-03-16 19:53:31 -0500

    testing database on server

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'laravel_db',
 			'username'  => 'root',
-			'password'  => 'password',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
