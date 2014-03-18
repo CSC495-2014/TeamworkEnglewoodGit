2186398f69f142e1190f89be9b6fc4c190eb91e7
apotheos <example@gmail.com>
2014-03-17 22:59:36 -0500

    temporarily removed route filters

--- app/routes.php
+++ app/routes.php
@@ -20,16 +20,22 @@ Route::post('login', 'LoginController@gitHubLoginPost');
 Route::get('login', 'LoginController@gitHubLoginPost');
 Route::get('logout', 'LoginController@logoutPost');
 
-Route::get('user/{user}/project/{project}/editor',
-		   array('before' => 'verifyUser:$user',
-		   function($user, $project)
-{
-	return View::make('editor', ['user' => $user, 'project' => $project]);
-}));
+// Remove filters for demo.
+//Route::get('user/{user}/project/{project}/editor',
+//		   array('before' => 'verifyUser:$user',
+//		   function($user, $project)
+//{
+//	return View::make('editor', ['user' => $user, 'project' => $project]);
+//}));
+//
+//Route::get('user/{user}/projects', array('before'=>'verifyUser', 'uses'=>'ProjectsController@display'));
 
-//array('before' => 'verifyUser:$user', 
+Route::get('user/{user}/project/{project}/editor', function($user, $project)
+{
+    return View::make('editor', ['user' => $user, 'project' => $project]);
+});
+
 Route::get('user/{user}/projects', 'ProjectsController@display');
-Route::get('user/{user}/projects', array('before'=>'verifyUser', 'uses'=>'ProjectsController@display'));
 
 Route::get('/user/{user}/project/{project}/is-cloned', 'GitController@isCloned');
 
