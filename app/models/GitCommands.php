760fe526c1ba8c1158841dffb971e578ebe8cb7d
ZAM- <zachary.mance@gmail.com>
2014-04-13 00:28:53 -0500

    Trying to debug the project path for cloning.

--- app/models/GitCommands.php
+++ app/models/GitCommands.php
@@ -96,7 +96,7 @@ class GitCommands extends AbstractFileSystem
 	{
 		// example SSH URL - git@github.com:ZAM-/TestRepo.git'
 		$repoURL = 'git@github.com:' . $this->getUserName() . '/' . $this->getProjectName() . '.git';
-		$projectPath = $this->getPath();
+		$projectPath = base_path() . AbstractFileSystem::ROOT . 'users/' . $this->getUserName() . '/projects/' . $this->getProjectName() . '/';
 		if (!file_exists($projectPath)) // if the project is not cloned, clone it
 		{
 			mkdir($projectPath, Config::get('filesystem.permissions.directory'));
